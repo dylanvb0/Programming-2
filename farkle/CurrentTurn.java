96ceae29eec478d8313330d47b86bba6982b8e27
Dylan Vander Berg <dylanvanderberg@gmail.com>
2017-02-26 21:57:31 -0600

    rough first implementation of game

--- farkle/CurrentTurn.java
+++ farkle/CurrentTurn.java
@@ -1,3 +1,10 @@
+/*Dylan Vander Berg and Dan Kelly
+*Farkle
+*Current Turn Class
+*Code written primarily by Dylan Vander Berg
+*Note - Even though there is more code here, it was a lot of similiar code with copy/paste and small revisions between methods
+ */
+
 package farkle;
 
 import java.util.ArrayList;
@@ -19,36 +26,45 @@ import java.util.LinkedHashMap;
 
 public class CurrentTurn {
 	private int[] dice;//int array for value of dice
-	private ArrayList<Integer> holdDice;
+	private boolean[] holdDice;
+	private boolean[] currentRollHold;
 	private int turnScore;
 	
-	//constructor - roll dice
+	//constructor
 	public CurrentTurn(){
 		dice = new int[6];
-		holdDice = new ArrayList<Integer>();
-		holdDice.add(2);
-		rollDice();
+		holdDice = new boolean[6];
+		currentRollHold = new boolean[6];
 	}
 	
-	public void rollDice(){
+	public boolean rollDice(){
 		System.out.print("Dice are: ");
 		for(int i = 0; i < 6; i++){
-			boolean diceExcluded = false;
-			for(int ii = 0; ii < holdDice.size(); ii++){
-				if(i == holdDice.get(ii)){
-					diceExcluded = true;
-				}
-			}
-			if(!diceExcluded){
+			if(!holdDice[i]){
 				dice[i] = (int)(Math.random() * 6) + 1;
+				System.out.print(dice[i] + ", ");
+			}else{
+				System.out.print("X, ");
 			}
-			System.out.print(dice[i] + ", ");
+			
+			
 		}
-		checkFarkle();
+		return !checkFarkle();
 	}
 	
+	public void holdDice(ArrayList<Integer> holdList){
+		for(int i = 0; i < holdList.size(); i++){
+			currentRollHold[holdList.get(i)] = true;
+		}
+	}
+	
+	public int getTurnScore(){
+		return this.turnScore;
+	}
+	
+	
 	//check farkle
-	private void checkFarkle(){
+	private boolean checkFarkle(){
 		boolean farkle = true;
 		farkle = (checkThreeOfAKind()) ? false : farkle;
 		farkle = (checkOnes()) ? false : farkle;
@@ -59,13 +75,13 @@ public class CurrentTurn {
 		farkle = (checkFiveOfAKind()) ? false : farkle;
 		farkle = (checkSixOfAKind()) ? false : farkle;
 		
-		if(farkle){
-			System.out.println("Farkle");
-		}
+		return farkle;
 	}
+	
+	/*BEGIN - Check Farkle Methods*/
 
 	private boolean checkSixOfAKind() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] == 6){
 				return true;
@@ -75,7 +91,7 @@ public class CurrentTurn {
 	}
 
 	private boolean checkFiveOfAKind() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] >= 5){
 				return true;
@@ -85,7 +101,7 @@ public class CurrentTurn {
 	}
 
 	private boolean checkThreePairs() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		int numPairs = 0;
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] >= 2){
@@ -96,7 +112,7 @@ public class CurrentTurn {
 	}
 
 	private boolean checkStraight() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] != 1){
 				return false;
@@ -106,7 +122,7 @@ public class CurrentTurn {
 	}
 
 	private boolean checkFourOfAKind() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] >= 4){
 				return true;
@@ -116,17 +132,17 @@ public class CurrentTurn {
 	}
 
 	private boolean checkFives() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		return numOfNums[4] >= 1;//check if any 5s (index 4 is 5s)
 	}
 
 	private boolean checkOnes() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		return numOfNums[0] >= 1;//check if any 1s (index 0 is 1s)
 	}
 
 	private boolean checkThreeOfAKind() {
-		int[] numOfNums = getNumOfNums();
+		int[] numOfNums = getNumOfNumsCheck();
 		for(int i = 0; i < 6; i++){
 			if(numOfNums[i] >= 3){
 				return true;
@@ -145,23 +161,190 @@ public class CurrentTurn {
 		return returnArray;
 	}
 	
-	private int[] getNumOfNums(){
+	private int[] getNumOfNumsCheck(){
 		int[] numOfNums = new int[6];//array to count the amount of each number(i.e. index 0 is num of 1s)
-		for(int i = 0; i < 6; i++){
-			numOfNums[dice[i] - 1]++;
+		for(int i = 0; i < dice.length; i++){
+			if(!holdDice[i]){
+				numOfNums[dice[i] - 1]++;
+			}
 		}
 		return numOfNums;
 	}
 	
-	//method check 3 of a kind - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check ones - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check fives - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check straight - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check 3 pairs - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check 4 of a kind - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check 5 of a kind - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
-	//method check 6 of a kind - return HashMap<Integer, ArrayList<Integer>>//key, num of points earned, ArrayList<Integer>, indices of dice needed for points, return null for none
+	/*END - Check Farkle Methods*/
 	
+	/*BEGIN - Scoring Methods*/
+	
+	private int[] getNumOfNumsScore(){
+		int[] numOfNums = new int[6];//array to count the amount of each number(i.e. index 0 is num of 1s)
+		for(int i = 0; i < dice.length; i++){
+			if(currentRollHold[i]){
+				numOfNums[dice[i] - 1]++;
+			}
+		}
+		return numOfNums;
+	}
+	
+	private int scoreSixOfAKind() {
+		int[] numOfNums = getNumOfNumsScore();
+		for(int i = 0; i < numOfNums.length; i++){
+			if(numOfNums[i] == 6){
+				for(int ii = 0; ii < dice.length; ii++){
+					holdDice[ii] = true;
+					currentRollHold[ii] = false;
+				}
+				return 3000;
+			}
+		}
+		return 0;
+	}
+	
+	private int scoreFiveOfAKind() {
+		int[] numOfNums = getNumOfNumsScore();
+		for(int i = 0; i < numOfNums.length; i++){
+			if(numOfNums[i] == 5){
+				for(int ii = 0; ii < dice.length; ii++){
+					if(dice[ii] == i + 1){
+						holdDice[ii] = true;
+						currentRollHold[ii] = false;
+					}
+				}
+				return 2000;
+			}
+		}
+		return 0;
+	}
+	
+	private int scoreFourOfAKind() {
+		int[] numOfNums = getNumOfNumsScore();
+		for(int i = 0; i < numOfNums.length; i++){
+			if(numOfNums[i] == 4){
+				int numUsed = 0;
+				for(int ii = 0; ii < dice.length; ii++){
+					if(dice[ii] == i + 1 && currentRollHold[ii] && numUsed < 4){
+						holdDice[ii] = true;
+						currentRollHold[ii] = false;
+						numUsed++;
+					}
+				}
+				return 1000;
+			}
+		}
+		return 0;
+	}
+	
+	private int scoreThreeOfAKind(){
+		int combinationScore = 0;
+		int[] numOfNums = getNumOfNumsScore();
+		if(numOfNums[0] >= 3){
+			combinationScore += 1000;//award 1000 points for 3 ones
+			int numUsed = 0;
+			for(int i = 0; i < dice.length; i++){
+				if(dice[i] == 1 && currentRollHold[i] && numUsed < 3){
+					holdDice[i] = true;
+					currentRollHold[i] = false;//remove dice so they aren't scored twice
+					numUsed++;
+				}
+			}
+		}
+		for(int i = 1; i < numOfNums.length; i++){
+			if(numOfNums[i] >= 3){
+				combinationScore += (i + 1) * 100; //add 100 times dice value for other 3 of a kinds
+				int numUsed = 0;
+				for(int ii = 0; ii < dice.length; ii++){
+					if(dice[ii] == i + 1 && currentRollHold[ii] && numUsed < 3){
+						holdDice[ii] = true;
+						currentRollHold[ii] = false;//remove dice so they aren't scored twice
+						numUsed++;
+					}
+				}
+			}
+		}
+		return combinationScore;
+		
+	}
+	
+	public int scoreStraight(){
+		int[] numOfNums = getNumOfNumsScore();
+		for(int i = 0; i < numOfNums.length; i++){
+			if(numOfNums[i] != 1){
+				return 0;
+			}
+		}
+		for(int i = 0; i < holdDice.length; i++){
+			holdDice[i] = true;
+			currentRollHold[i] = false;
+		}
+		return 3000;
+	}
+	
+	private int scoreThreePairs() {
+		int numPairs = 0;
+		int[] numOfNums = getNumOfNumsScore();
+		for(int i = 0; i < numOfNums.length; i++){
+			if(numOfNums[i] == 4){
+				numPairs += 2;
+				for(int ii = 0; ii < dice.length; ii++){
+					if(dice[ii] == i + 1 && currentRollHold[ii]){
+						holdDice[ii] = true;
+						currentRollHold[ii] = false;
+					}
+				}
+			}else if(numOfNums[i] == 2){
+				numPairs++;
+				for(int ii = 0; ii < dice.length; ii++){
+					if(dice[ii] == i + 1 && currentRollHold[ii]){
+						holdDice[ii] = true;
+						currentRollHold[ii] = false;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	private int scoreOnes() {
+		int combinationScore = 0;
+		for(int i = 0; i < dice.length; i++){
+			if(dice[i] == 1 && currentRollHold[i]){
+				holdDice[i] = true;
+				currentRollHold[i] = false;
+				combinationScore += 100;
+			}
+		}
+		return combinationScore;
+	}
+	
+	private int scoreFives() {
+		int combinationScore = 0;
+		for(int i = 0; i < dice.length; i++){
+			if(dice[i] == 5 && currentRollHold[i]){
+				holdDice[i] = true;
+				currentRollHold[i] = false;
+				combinationScore += 50;
+			}
+		}
+		return combinationScore;
+	}
+	
+	/*END - Scoring Methods*/
+
+	public void scoreTurn() {
+		turnScore += scoreSixOfAKind();
+		turnScore += scoreStraight();
+		turnScore += scoreFiveOfAKind();
+		turnScore += scoreThreePairs();
+		turnScore += scoreFourOfAKind();
+		turnScore += scoreThreeOfAKind();
+		turnScore += scoreOnes();
+		turnScore += scoreFives();
+		System.out.println("Current Score is: " + turnScore);
+	}
+
+	
+
+	
+
 	//score roll based on user selection
 	
 	
